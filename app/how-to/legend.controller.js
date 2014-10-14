d3f5b723739ffb86f179995b400843e6fdd4e938
soneill <soneill@scottlogic.co.uk>
2014-10-14 17:40:30 +0100

    updating the controller to use the latest structure

--- app/how-to/legend.controller.js
+++ app/how-to/legend.controller.js
@@ -5,54 +5,52 @@
 {
     'use strict';
 
-    angular.module('insightChartsControllers').controller('HowToLegend', ['$scope', '$http',
-        function($scope, $http)
-        {
+    function LegendController () {
+        Prism.highlightAll();
 
-            Prism.highlightAll();
+        var data = [
+            { "name": "Michelle Hopper", "age": 26, "eyeColor": "green" },
+            { "name": "Cochran Mcfadden", "age": 22, "eyeColor": "green" },
+            { "name": "Jessie Mckinney", "age": 23, "eyeColor": "brown" },
+            { "name": "Rhoda Reyes", "age": 40, "eyeColor": "brown" },
+            { "name": "Hawkins Wolf", "age": 26, "eyeColor": "green" },
+            { "name": "Lynne O'neill", "age": 39, "eyeColor": "green" },
+            { "name": "Twila Melendez", "age": 26, "eyeColor": "blue" },
+            { "name": "Courtney Diaz", "age": 20, "eyeColor": "brown" },
+            { "name": "Burton Beasley", "age": 36, "eyeColor": "green" },
+            { "name": "Mccoy Gray", "age": 25, "eyeColor": "brown" },
+            { "name": "Janie Benson", "age": 30, "eyeColor": "green" },
+            { "name": "Cherie Wilder", "age": 30, "eyeColor": "green" }
+        ];
 
-            var data = [
-                { "name": "Michelle Hopper", "age": 26, "eyeColor": "green" },
-                { "name": "Cochran Mcfadden", "age": 22, "eyeColor": "green" },
-                { "name": "Jessie Mckinney", "age": 23, "eyeColor": "brown" },
-                { "name": "Rhoda Reyes", "age": 40, "eyeColor": "brown" },
-                { "name": "Hawkins Wolf", "age": 26, "eyeColor": "green" },
-                { "name": "Lynne O'neill", "age": 39, "eyeColor": "green" },
-                { "name": "Twila Melendez", "age": 26, "eyeColor": "blue" },
-                { "name": "Courtney Diaz", "age": 20, "eyeColor": "brown" },
-                { "name": "Burton Beasley", "age": 36, "eyeColor": "green" },
-                { "name": "Mccoy Gray", "age": 25, "eyeColor": "brown" },
-                { "name": "Janie Benson", "age": 30, "eyeColor": "green" },
-                { "name": "Cherie Wilder", "age": 30, "eyeColor": "green" }
-            ];
+        var dataset = new insight.DataSet(data);
 
-            var dataset = new insight.DataSet(data);
+        var chart = new insight.Chart('Ages', '#chart')
+            .width(500)
+            .height(350)
+            .title('Ages of People')
+            .legend(new insight.Legend());
 
-            var chart = new insight.Chart('Ages', '#chart')
-                .width(500)
-                .height(350)
-                .title('Ages of People')
-                .legend(new insight.Legend());
+        var x = new insight.Axis('Age', insight.scales.linear);
+        var y = new insight.Axis('', insight.scales.ordinal);
 
-            var x = new insight.Axis('Age', insight.scales.linear);
-            var y = new insight.Axis('', insight.scales.ordinal);
-
-            chart.xAxis(x);
-            chart.yAxis(y);
+        chart.xAxis(x);
+        chart.yAxis(y);
 
 
-            var rows = new insight.RowSeries('rows', dataset, x, y)
-                .keyFunction(function (person) {
-                    return person.name;
-                })
-                .valueFunction(function (person) {
-                    return person.age;
-                });
+        var rows = new insight.RowSeries('rows', dataset, x, y)
+            .keyFunction(function (person) {
+                return person.name;
+            })
+            .valueFunction(function (person) {
+                return person.age;
+            });
 
 
-            chart.series([rows]);
+        chart.series([rows]);
 
-            chart.draw();
-        }
-    ]);
+        chart.draw();
+    }
+
+    angular.module('insightChartsControllers').controller('HowToLegend', [LegendController]);
 }());
