b94b4f67dadaf19314141265b7bcb32197d6b5da
Matthew Dunsdon <mdunsdon@scottlogic.com>
2014-10-15 10:59:54 +0100

    Add API docs and how-to-guide directives
    Placed directives in 'components' directory

--- app/app.dist.js
+++ app/app.dist.js
@@ -416,6 +416,49 @@ function createLanguageChart(chartGroup, languages){
 }
 
 
+(function() {
+
+    'use strict';
+
+    /*
+     * Listens to elements that change content and highlights the syntax
+     */
+    function apiDocsDirective() {
+        return {
+            restrict: 'E',
+            transclude: true,
+            scope: {
+                page: '@',
+                anchor: '@'
+            },
+            templateUrl: '/app/components/api-docs.directive.html'
+        };
+    }
+
+    angular.module('insightCharts').directive('apiDocs', apiDocsDirective);
+})();
+
+(function() {
+
+    'use strict';
+
+    /*
+     * Listens to elements that change content and highlights the syntax
+     */
+    function howToGuideDirective() {
+        return {
+            restrict: 'E',
+            transclude: true,
+            scope: {
+                page: '@'
+            },
+            templateUrl: '/app/components/how-to-guide.directive.html'
+        };
+    }
+
+    angular.module('insightCharts').directive('howToGuide', howToGuideDirective);
+})();
+
 (function () {
     'use strict';
 
