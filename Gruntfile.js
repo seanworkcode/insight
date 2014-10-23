c8526c2784a4b491271e2ee5159632c7a55eb3d1
Matthew Dunsdon <mdunsdon@scottlogic.com>
2014-10-23 16:22:45 +0100

    Add test-runner grunt task
    Allows tests to be run in the browser without building the zip
    each time

--- Gruntfile.js
+++ Gruntfile.js
@@ -127,11 +127,20 @@ module.exports = function(grunt) {
         }
     },
     watch: {
-      files: ['<%= jshint.files %>', 'tests/**/*.spec.js'],
-      tasks: ['deploy'],
-      options: {
-          livereload: true
-      }
+        deploy: {
+            files: ['<%= jshint.files %>', 'tests/**/*.spec.js'],
+            tasks: ['deploy'],
+            options: {
+                livereload: true
+            }
+        },
+        test: {
+            files: ['<%= jshint.files %>', 'tests/**/*.spec.js'],
+            tasks: ['test'],
+            options: {
+                livereload: true
+            }
+        }
     },
     compress: {
         zip: {
@@ -169,9 +178,11 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-jsdoc');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-compress');
-  
 
+
+  grunt.registerTask('test', ['jsbeautifier', 'jshint', 'jasmine']);
   
-  grunt.registerTask('deploy', ['jsbeautifier', 'jshint', 'jasmine', 'concat', 'uglify', 'cssmin', 'clean', 'jsdoc', 'compress']);
-  grunt.registerTask('default', ['deploy', 'connect:server','open','watch']);
+  grunt.registerTask('deploy', ['test', 'concat', 'uglify', 'cssmin', 'clean', 'jsdoc', 'compress']);
+  grunt.registerTask('default', ['deploy', 'connect:server', 'open', 'watch:deploy']);
+  grunt.registerTask('test-runner', ['test', 'connect:server', 'open', 'watch:test']);
 };
