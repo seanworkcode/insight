bc6b96e58299abd0432e7502298de7a75fb2da07
soneill <soneill@scottlogic.co.uk>
2014-10-30 16:19:45 +0000

    refactored gallery to load index files

--- app/gallery/gallery.js
+++ app/gallery/gallery.js
@@ -10,42 +10,34 @@
 
         $scope.currentItem = undefined;
 
+        $scope.setTemplate = function(item) {
+            var oldTemplate = $scope.template;
+            $scope.template = 'app/gallery/items/' + item.index;
+            if (oldTemplate !== $scope.template) {
+                $scope.loadItem(item);
+            }
+        };
+
         $scope.loadItems = function() {
             $scope.items = [
                 {
+                    index: 'barchart/index.html',
                     title: 'Awesome bar chart!',
-                    thumbnail: 'barchart/thumbnail.png',
-                    dataPath: 'barchart/data.json',
-                    script: 'barchart/script.js',
-                    desc: 'As simple as it gets. A bar chart for viewing one data set against the other.'
+                    thumbnail: 'barchart/thumbnail.png'
                 },
                 {
+                    index: 'interactive/index.html',
                     title: '~ Zooming chart ~',
-                    thumbnail: 'interactive/thumbnail.png',
-                    dataPath: 'interactive/data.json',
-                    script: 'interactive/script.js',
-                    desc: 'Interactive chart, try scrolling while the mouse is hovered over the chart to watch the scale change'
+                    thumbnail: 'interactive/thumbnail.png'
                 }
             ];
+            $scope.setTemplate($scope.items[0]);
         };
 
         $scope.loadItem = function(item) {
-            $scope.currentItem = item;
-            $scope.loadData(item.dataPath).then(function(data) {
-                $scope.loadCode(item.script).then(function(code) {
-                    $scope.displayChart(data, code);
-                    $scope.displayCode(code);
-                    $scope.showGallery = false;
-                });
+            $scope.loadCode(item.index).then(function(code) {
+                $scope.displayCode(code);
             });
-        };
-
-        $scope.loadData = function(dataPath) {
-            var deferred = $q.defer();
-            d3.json('app/gallery/items/' + dataPath, function(data) {
-                deferred.resolve(data);
-            });
-            return deferred.promise;
         };
 
         $scope.loadCode = function(filePath) {
@@ -58,17 +50,9 @@
             return deferred.promise;
         };
 
-        $scope.displayChart = function(data, code) {
-            var script = document.createElement('script');
-            script.src = 'data:text/javascript,' + encodeURI(code);
-            document.body.appendChild(script);
-            script.onload = function() {
-                chartingFunction.call(this, data.data, '#gallery-chart');
-            };
-        };
-
         $scope.displayCode = function(code) {
-            angular.element('#gallery-code').html('<code id="codeItem" class="language-javascript loading">' + code + '</code>');
+            // angular.element('#gallery-code').html('<code id="codeItem" class="language-javascript loading">' + code + '</code>');
+            angular.element('#gallery-code').text(code);
             Prism.highlightAll();
         };
 
@@ -77,6 +61,7 @@
             angular.element('#gallery-chart').empty();
             angular.element('#gallery-code').empty();
         };
+
     }
 
     angular.module('insightChartsControllers').controller('Gallery', ['$scope', '$http', '$q', galleryController]);
